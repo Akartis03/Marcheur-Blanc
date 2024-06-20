0aec50800779bb8b33f2b21db52491de8d7455bc
RSitrakaFL <hei.sitraka@gmail.com>
2024-06-20 23:26:59 +0300

    feat: can go to the target without map

--- src/main/java/school/hei/marcheurblanc/Marcheur.java
+++ src/main/java/school/hei/marcheurblanc/Marcheur.java
@@ -3,27 +3,39 @@ package school.hei.marcheurblanc;
 import lombok.Data;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Data
 @ToString
 public class Marcheur {
     private final String nom;
     private Lieu position;
-    private List<Lieu> lieusDejaVisite;
+    private Set<Lieu> lieusDejaVisite;
 
     public Marcheur(String nom, Lieu position){
         this.nom = nom;
         this.position = position;
-        this.lieusDejaVisite = new ArrayList<>();
+        this.lieusDejaVisite = new HashSet<>();
         this.lieusDejaVisite.add(position);
+    }
+
+    public List<String> trouverCheminAleatoire(Lieu destination){
+        List<String> chemin = new ArrayList<>();
+        var depart = this.position;
+        chemin.add(depart.getNom());
+        if (destination.equals(depart)){
+            return chemin;
+        }
+        while (position != destination){
+            chemin.add(marcher().getNom());
+        }
+        return chemin;
     }
 
     public Lieu marcher(){
         var rueChoisi = choisirRueAleatoire(position);
         position = rueChoisi.getAutreLieu(position);
+        ajoutLieuVisite(position);
         return position;
     }
     public Rue choisirRueAleatoire(Lieu lieu){
--- src/test/java/school/hei/marcheurblanc/MarcheurTest.java
+++ src/test/java/school/hei/marcheurblanc/MarcheurTest.java
@@ -1,6 +1,5 @@
 package school.hei.marcheurblanc;
 
-import lombok.extern.flogger.Flogger;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,15 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MarcheurTest{
     @Test
     void le_marcheur_peut_marcher(){
-        var Sekolintsika = new Lieu("Sekolintsika");
-        var Hei = new Lieu("Hei");
+        var sekolintsika = new Lieu("Sekolintsika");
+        var hei = new Lieu("Hei");
 
-        var sekolintsikaHei = new Rue("Sekolintsika & Hei", Sekolintsika, Hei);
+        var sekolintsikaHei = new Rue("Sekolintsika & Hei", sekolintsika, hei);
 
-        var bjarni = new Marcheur("Bjarni",Sekolintsika);
+        var bjarni = new Marcheur("Bjarni",sekolintsika);
         var depart = bjarni.getPosition();
         var positionDeBjarniDeplace = bjarni.marcher();
 
         assertNotEquals(depart, positionDeBjarniDeplace);
+    }
+
+    @Test
+    void peut_aller_vers_destination(){
+        var marais = new Lieu("Marais");
+        var sekolintsika = new Lieu("Sekolintsika");
+        var hei = new Lieu("Hei");
+        var pullman = new Lieu("Pullman");
+        var balancoire = new Lieu("Balancoire");
+        var nexta = new Lieu("Nexta");
+        var boulevard = new Lieu("Boulevard");
+        var esti = new Lieu("ESTI");
+
+        var maraisSekolintsika = new Rue("Marais & Sekolintsika", marais, sekolintsika);
+        var sekolintsikaHei = new Rue("Sekolintsika & Hei", sekolintsika, hei);
+        var heiPullman = new Rue("Hei & Pullman", hei, pullman);
+        var heiBalancoire = new Rue("Hei & Balancoire", hei, balancoire);
+        var pullmanNexta = new Rue("Pullman & Nexta", pullman, nexta);
+        var balancoireBoulevard = new Rue("Balancoire & Boulevard", balancoire, boulevard);
+        var balancoireEsti = new Rue("Balancoire & ESTI", balancoire, esti);
+        var boulevardEsti = new Rue("Boulevard & ESTI", boulevard, esti);
+
+        var bjarni = new Marcheur("Bjarni",sekolintsika);
+        var destination = nexta;
+        var chemin = bjarni.trouverCheminAleatoire(destination);
+        var leBoutDuChemin = chemin.get(chemin.size() - 1);
+        assertEquals(destination.getNom(), leBoutDuChemin);
     }
 }
